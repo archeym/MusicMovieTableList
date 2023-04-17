ee8fa88c05bfc3ef1f66efcbb2f339b975d89b40
Arkadijs Makarenko <arkadijs.makarenko@gmail.com>
2023-04-17 17:43:08 +0200

    init TableView

---
+++ TableList/Controller/MoviesTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  MoviesTableViewCell.swift
+//  TableList
+//
+//  Created by Arkadijs Makarenko on 17/04/2023.
+//
+
+import UIKit
+
+class MoviesTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var movieImageView: UIImageView!
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
